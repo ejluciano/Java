395f35c2eaa91ad4a6e1c7a0995ae132b5c47a36
ejluciano <ej.lucianojr@gmail.com>
2021-06-25 03:30:52 +0800

    Learning Java day 1

--- Guessing game/src/HiLo.java
+++ Guessing game/src/HiLo.java
@@ -5,6 +5,7 @@ public class HiLo {
         //System.in pulls the computer's input
         Scanner scan = new Scanner(System.in); 
         String playAgain = "";
+        //do-while loop
         do {
             //Generates random number
             int theNumber = (int)(Math.random() * 100 + 1);
@@ -13,7 +14,8 @@ public class HiLo {
             while (guess != theNumber){
                 System.out.println("Guess a number between 1 and 100: ");
                 guess = scan.nextInt();
-                if (guess < theNumber)
+                //nested loop
+                if (guess < theNumber) 
                     System.out.println(guess + " is too low. Try again.");
                 else if (guess > theNumber)
                      System.out.println(guess + " is to high. Try again.");
@@ -23,5 +25,7 @@ public class HiLo {
             System.out.println("Would you like to play again (y/n)?");
             playAgain = scan.next();
         } while (playAgain.equalsIgnoreCase("y"));
+            System.out.println("Thank you for playing! Goodbye.");
+            scan.close();
     }
 }
